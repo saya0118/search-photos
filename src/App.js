4882d32b8864a60cb62f3ac2f67be6fb15277835
ShosukeDoi <doiverson.canada@gmail.com>
2021-11-17 23:01:41 -0800

    update

--- src/App.js
+++ src/App.js
@@ -2,144 +2,77 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 import ImageList from './imageList';
-import FavList from './favList';
 
 class App extends React.Component{
-  state = {images: [], text: '', query:'apple', fav:[]}
-//   const [images, setImages] = useState([]);
-//   const [text, setText] = useState('');
-//   const [query, setQuery] = useState('apple');
+
+  state = {
+    images: [],
+    text: '',
+    query:'apple',
+  }
 
   componentDidMount(){
-
-    console.log(process.env);
-
     axios.get("https://api.unsplash.com/photos", {
-      params:{
-        per_page: 30
-      },
-
+      params:　{　per_page: 30　},
       headers: {
         Authorization: `Client-ID ${process.env.REACT_APP_CLIENT_ID}`
-        // process.envはREACT＿APPを自動で探してくれるよ！
+      }}
+    ).then((response) => {
+      const items = response.data.map(x => {
+        x.favorite = false;
+        return x
+      })
+      this.setState({images: items})
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState({query:this.state.text});
+
+    axios.get("https://api.unsplash.com/search/photos", {
+      params:{
+        query: this.state.text,
+        per_page: 30
+      },
+      headers:{
+        Authorization: `Client-ID ${process.env.REACT_APP_CLIENT_ID}`
       }
     }).then((response) => {
-      console.log(response);
-      this.setState({images: response.data})
+      this.setState({images: response.data.results})
     })
-    // getData(()=>{
-      // this.setState({images:items})
-    // })
-    // console.log('run useEffect');
-    
-  //   fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     console.log(data);
-  //     setImages(data.results)
-  //     })
-  // }, [query]
-}
+    this.setState({text:''});
+  }
 
-onSubmit = (e) => {
-  e.preventDefault();
-  this.setState({query:this.state.text});
-  axios.get("https://api.unsplash.com/search/photos", {
-    params:{
-      query: this.state.text,
-      per_page: 30
-    },
-    headers:{
-      Authorization: `Client-ID ${process.env.REACT_APP_CLIENT_ID}`
-    }
-  }).then((response) => {
-    console.log(response);
-    this.setState({images: response.data.results})
-  })
-  this.setState({text:''});
-
-  // console.log('called onSubmit');
-} 
-// catch (err) {
-  
-// }
+  onHandleLike = (index) => {
+    const duplicatedImages = [...this.state.images];
+    duplicatedImages[index].favorite = !duplicatedImages[index].favorite;
+    this.setState({
+      images: duplicatedImages
+    })
+  }
 
   render(){
-    console.log(this.state)
     return (
       <div className="App">
         <div className="main">
           <h1>Search your favorite photos!</h1>
           <form onSubmit={this.onSubmit}>
             <input
-            type="text"
-            onChange={e => this.setState({text: e.target.value})}
-            value={this.state.text}
+                type="text"
+                onChange={e => this.setState({text: e.target.value})}
+                value={this.state.text}
             />
-            <button type="submit">
-              Search
-            </button>
+            <button type="submit">Search</button>
           </form>
-        </div>  
-
-        <ImageList images={this.state.images}/>
+        </div>
+        <ImageList
+            images={this.state.images}
+            onHandleLike={this.onHandleLike}
+        />
       </div>
     )
   }
 }
-
-// function App() {
-//   const [images, setImages] = useState([]);
-//   const [text, setText] = useState('');
-//   const [query, setQuery] = useState('apple');
-
-//   useEffect(() => {
-//     console.log('run useEffect');
-//     fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log(data);
-//       setImages(data.results)
-//       })
-//   }, [query])
-
-//   const onSubmit = (e) => {
-//     e.preventDefault();
-//     setQuery(text);
-//     setText('');
-//     console.log('called onSubmit');
-//   }
-//   return (
-//     <div className="App">
-//       <div className="main">
-//         <form onSubmit={onSubmit}>
-//           <input
-//           type="text"
-//           onChange={e => setText(e.target.value)}
-//           value={text}
-//           />
-//           <button type="submit">
-//             Search
-//           </button>
-//         </form>
-//       </div>
-//       <div className="container">
-//         {
-//           images.map(image => (
-//             <div key={image.id} className="card">
-//               <img src={image.urls.regular} className="card-img" alt=""/>
-//               <div className="card-content">
-//                 <div className="card-title">
-//                 {image.alt_description}
-//               </div>
-//             </div>
-//             </div>
-//           ))
-//         }
-//       </div>   
-//     </div>
-//   )
-// }
-
 
 export default App;
--- src/imageList.js
+++ src/imageList.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React　from 'react';
 import Box from '@mui/material/Box';
 import List from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
@@ -8,76 +8,43 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
 function ImageList (props) {
-  const [favIcon, setFavIcon] = useState();
-  const [favImage, setFavImage] = useState([]);
-
-  useEffect(() => {
-    console.log('run useEffect');
-    // favIcon === <FavoriteIcon/> ? 
-    // this.setState{{favImage:this.state.images}} :
-    
-    // IconButton === <FavoriteIcon/> ? <FavoriteBorderIcon/> : <FavoriteIcon/>
-  }, [favIcon])
-
-  const onClick = (e) => {
-        console.log("click");
-        e.preventDefault();
-        favIcon === <FavoriteIcon/> ? <FavoriteBorderIcon/> : <FavoriteIcon/>
-        // this.setState({favImage:this.state.images.id})      
-      }
-
-    return(
+    return　(
         <div className="container">
             <Box sx={{ width: "100%", height: "100vh", overflowY: 'scroll' }}>
-            <List variant="masonry"cols={2} gap={8}>
-                {props.images.map((image) => (
-            <ImageListItem key={image.id}>
-            <img
-              src={`${image.urls.regular}?w=248&fit=crop&auto=format`}
-              srcSet={`${image.urls.regular}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={image.alt_description}
-              loading="lazy"
-            />
-            <ImageListItemBar
-            sx={{
-              background:
-                'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-                'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-            }}
-            position="top"
-            actionIcon={
-              <IconButton
-                sx={{ color: 'white' }}
-                aria-label={`star ${image.title}`}
-                onClick={onClick}
-              >
-                <FavoriteBorderIcon/>
-              </IconButton>
-            }
-            actionPosition="left"
-          />
-            </ImageListItem>
-            ))}
+            <List variant="masonry" cols={2} gap={8}>
+                {props.images.map((image, i) => (
+                    <ImageListItem key={image.id}>
+                        <img
+                          src={`${image.urls.regular}?w=248&fit=crop&auto=format`}
+                          srcSet={`${image.urls.regular}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                          alt={image.alt_description}
+                          loading="lazy"
+                        />
+                        <ImageListItemBar
+                            sx={{
+                              background:
+                                'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+                                'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+                            }}
+                            position="top"
+                            actionPosition="left"
+                            actionIcon={
+                              <IconButton
+                                sx={{ color: 'white' }}
+                                aria-label={`star ${image.title}`}
+                                onClick={() => props.onHandleLike(i)}
+                              >
+                                  {
+                                      image.favorite ? <FavoriteIcon style={{fill: "#ff284e"}}/> : <FavoriteBorderIcon/>
+                                  }
+                              </IconButton>
+                            }
+                      />
+                    </ImageListItem>
+                ))}
             </List>
             </Box>
-
-        {/* {
-          props.images.map(image => (
-            <div key={image.id} className="card">
-              <img src={image.urls.regular} className="card-img" alt=""/>
-              <div className="card-content">
-                <div className="card-title">
-                {image.alt_description}
-              </div>
-            </div>
-            </div>
-          ))
-        } */}
-      </div>   
-
-
-
-
+        </div>
     )
 }
 
